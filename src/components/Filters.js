e0f23604c5e3bd0d72e6531ddee924d45de4e1b1
Thomas Boos Pegler <tboospegler@gmail.com>
2022-09-21 14:49:24 -0400

    req 7 - finalizado

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -16,58 +16,93 @@ export default function Filters() {
 
   return (
     <section>
-      <input
-        data-testid="name-filter"
-        onChange={ (event) => setNameFilter({
-          filterByName: {
-            name: event.target.value,
-          },
-        }) }
-        value={ nameFilter.filterByName.name }
-      />
-      <select
-        data-testid="column-filter"
-        onChange={ (({ target }) => setColumnValue(target.value)) }
-      >
-        { columnFilterOptions.map((option, index) => (
-          <option key={ index } value={ option }>{option}</option>
-        ))}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        onChange={ (({ target }) => setOperatorValue(target.value)) }
-      >
-        { operatorFilterOprions.map((option, index) => (
-          <option key={ index } value={ option }>{option}</option>
-        ))}
-      </select>
-      <input
-        data-testid="value-filter"
-        type="number"
-        onChange={ ({ target }) => setFilterValue(target.value) }
-        value={ filterValue }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ () => {
-          const optionIndex = columnFilterOptions.indexOf(columnValue);
-          columnFilterOptions.splice(optionIndex, 1);
-          setColumnFilterOptions(columnFilterOptions);
-          return setfilterByNumValue({
-            filterByNumericValues: [
-              ...filterByNumValue.filterByNumericValues,
-              {
-                column: columnValue,
-                comparison: operatorValue,
-                value: filterValue,
-              },
-            ],
-          });
-        } }
-      >
-        Filtrar
-      </button>
+      <div>
+        <input
+          data-testid="name-filter"
+          onChange={ (event) => setNameFilter({
+            filterByName: {
+              name: event.target.value,
+            },
+          }) }
+          value={ nameFilter.filterByName.name }
+        />
+        <select
+          data-testid="column-filter"
+          onChange={ (({ target }) => setColumnValue(target.value)) }
+        >
+          { columnFilterOptions.map((option, index) => (
+            <option key={ index } value={ option }>{option}</option>
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ (({ target }) => setOperatorValue(target.value)) }
+        >
+          { operatorFilterOprions.map((option, index) => (
+            <option key={ index } value={ option }>{option}</option>
+          ))}
+        </select>
+        <input
+          data-testid="value-filter"
+          type="number"
+          onChange={ ({ target }) => setFilterValue(target.value) }
+          value={ filterValue }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => {
+            const optionIndex = columnFilterOptions.indexOf(columnValue);
+            columnFilterOptions.splice(optionIndex, 1);
+            setColumnFilterOptions(columnFilterOptions);
+            setfilterByNumValue({
+              filterByNumericValues: [
+                ...filterByNumValue.filterByNumericValues,
+                {
+                  column: columnValue,
+                  comparison: operatorValue,
+                  value: filterValue,
+                },
+              ],
+            });
+            setColumnValue(columnFilterOptions[0]);
+            setOperatorValue(operatorFilterOprions[0]);
+            setFilterValue('0');
+          } }
+        >
+          Filtrar
+        </button>
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          onClick={ () => {
+            setfilterByNumValue({ filterByNumericValues: [] });
+            setColumnFilterOptions(options);
+          } }
+        >
+          Remover Filtros
+        </button>
+      </div>
+      <div>
+        {filterByNumValue.filterByNumericValues
+          .map(({ column, comparison, value }, index) => (
+            <div key={ index } data-testid="filter">
+              <span>{`${column} ${comparison} ${value}`}</span>
+              <button
+                type="button"
+                onClick={ () => {
+                  const filters = filterByNumValue.filterByNumericValues
+                    .filter((filter) => filter.column !== column);
+                  console.log(filters);
+                  setfilterByNumValue({ filterByNumericValues: filters });
+                  setColumnFilterOptions([...columnFilterOptions, column]);
+                } }
+              >
+                🗑
+              </button>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
